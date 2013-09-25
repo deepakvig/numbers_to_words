7f0d662b7659e90748ef80fa321f44b1813ba6dc
deepakvig@gmail.com <deepakvig@gmail.com>
2013-09-25 17:30:06 +0530

    Base App added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NumbersToWords::Application.config.secret_key_base = '20f149db57012ec08774231001a9061c12770f079720860a250a9704db3eea9683c30e7620216d6193985df67f5611c9de7079c394ca6c2ff7ca12c1564bc17f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NumbersToWords::Application.config.session_store :cookie_store, key: '_numbers_to_words_session'
